05915424ff59ab226ca89aa42762066457d93d7e
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-04-22 09:58:13 +0300

    basic-calc

--- 1. Basic/3) basic-calc/index.js
+++ 1. Basic/3) basic-calc/index.js
@@ -1,5 +1,18 @@
 function basicCalc(operation, a, b) {
   // Напиши свой код здесь
+    switch (operation){
+        case "-":
+            return a - b;
+        case "*":
+            return a * b;
+        case "+":
+            return a + b;
+        case "/":
+            return a / b;
+        default:
+            break;
+    }
+    return basicCalc;
 }
 
 window.basicCalc = basicCalc;
