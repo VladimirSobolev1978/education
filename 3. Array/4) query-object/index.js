d49c0e88c89c831e9f150357c34e67c5e486b947
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-04-27 17:21:47 +0300

    query-object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,5 +1,19 @@
 function queryToObject(query) {
   // Напиши свой код здесь
+    const paramsArr = query.replace('?', "").split('&');
+    const result = {}
+    for(let j = 0; j < paramsArr.length; j++) {
+        const params = paramsArr[j].split('=');
+        if(!Number.isNaN(Number(params[1]))) {
+            params[1] = Number(params[1])
+        }
+        if(params[1] === 'true' || params[1] === 'false') {
+            params[1] = params[1] === 'true';
+        }
+        result[params[0]] = params[1];
+    }
+    return result;
+
 };
 
 window.queryToObject = queryToObject;
