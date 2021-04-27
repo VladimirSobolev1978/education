e8dac91422c440c976f3a3195b60274e5081ace1
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-04-27 10:46:33 +0300

    full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,16 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+    // Напиши свой код здесь
+    let sum = 0;
+    if (!args.length) {
+        return 0;
+    }
+    for (let i = 0; i < args.length; i++) {
+        if (typeof args[i] !== 'number') {
+            throw new Error('Wrong Argument Type');
+        }
+        sum += args[i];
+    }
+    return sum;
 }
 
 window.fullSum = fullSum;
