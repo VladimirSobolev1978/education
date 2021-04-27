7eaf38cde5941ac62ae9396ab2461dd7eaa1a86d
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-04-27 13:29:53 +0300

    find-pair

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,5 +1,14 @@
 function findPair(arr) {
   // Напиши свой код здесь
+    for (let i = 0; i < arr.length; i++) {
+        let arg = arr[i];
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] === arg) {
+                return arg;
+            }
+        }
+    }
+    return null;
 };
 
 window.findPair = findPair;
