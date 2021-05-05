1c0be81adff1ca3c65cbef6f49167a1825564822
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-05-05 13:50:05 +0300

    async-sum

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,5 +1,18 @@
 function asyncSum(a, b) {
   // Напиши свой код здесь
+    let promise = new Promise(function(resolve, reject) {
+       if (typeof a === "number" && typeof b === "number") {
+           setTimeout(() => resolve(a + b), 1000);
+       } else {
+           reject(new Error("Не является числом!"));
+       }
+    });
+
+    promise.then(
+        result => result,
+        error => error
+    );
+    return promise;
 }
 
 window.asyncSum = asyncSum;
