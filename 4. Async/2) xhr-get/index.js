731667a0e997f3138b5ab6809589183264737417
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-05-06 15:37:11 +0300

    xhr-get

--- 4. Async/2) xhr-get/index.js
+++ 4. Async/2) xhr-get/index.js
@@ -1,5 +1,28 @@
 function xhrGet(url) {
-  // Напиши свой код здесь
+    // Напиши свой код здесь
+    let promise = new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.responseType = 'json';
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                resolve(xhr.response);
+            }
+            if (xhr.status === 201) {
+                resolve(xhr.response);
+            }
+            if (xhr.status === 400) {
+                reject({message: "client error"});
+            }
+            if (xhr.status === 500) {
+                reject({message: "server error"});
+            }
+        };
+        xhr.send();
+    });
+
+    return promise;
 }
 
 window.xhrGet = xhrGet;
