9d3587b6e92407bab90a7435941b01f464d08f2d
VladimirSobolev1978 <sobolev.exceedteam@gmail.com>
2021-05-11 16:52:35 +0300

    string-reverse

--- 5. Others/1) string-reverse/index.js
+++ 5. Others/1) string-reverse/index.js
@@ -6,3 +6,19 @@
   'bob'.reverse() === 'bob';
   'abc123'.reverse() === '321cba';
  */
+
+function stringReverse(str) {
+    if (!str) {
+        str = this;
+    }
+    let arrStrReverse = [];
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i]
+        arrStrReverse.push(letter);
+    }
+    arrStrReverse.reverse()
+    let strReverse = arrStrReverse.join('')
+    return strReverse
+}
+
+String.prototype.reverse = stringReverse;
